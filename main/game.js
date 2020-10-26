4b683e47b4c67c844972a54bc0e2eeae9d409604
kerzox <williampappin@gmail.com>
2020-10-26 13:30:52 +1000

    Added some functionality to gol. Ability to change neighbourhood type and order.

--- main/game.js
+++ main/game.js
@@ -1,11 +1,10 @@
-function nextGeneration() {
+function nextGeneration(type) {
     buffer = make2DArray(rows, cols);
 	for (let x = 0; x < rows; x++) {
 		for (let y = 0; y < cols; y++) {
             var cell = universe[x][y];
             var PREV_STATE = cell.state;
-            var neighbours = getNeighbours(universe, x, y);
-            //console.log(PREV_STATE)
+            var neighbours = getNeighbours(type,universe, x, y);
             buffer[x][y] = new Cell(x * resolution, y * resolution, resolution, cellState(neighbours, PREV_STATE));
         }
     }
@@ -24,20 +23,40 @@ function cellState(neighbours, PREV_STATE) {
     return STATE;
 }
 
-function getNeighbours(universe, pX, pY) {
+function getNeighbours(type, universe, pX, pY) {
     let sum = 0;
-	for (let x = pX - 1; x <= pX + 1; x++) {
-		for (let y = pY - 1; y <= pY + 1; y++) {
-            var nI = (x % rows + rows) % rows;
-            var nJ = (y % cols + cols) % cols;
-            var cell = universe[nI][nJ];
-
-            if (x != pX || y != pY ) {
-                sum += cell.state;
+    if (type == "moore") {
+        for (let x = pX - order; x <= pX + order; x++) {
+            for (let y = pY - order; y <= pY + order; y++) {
+                var nI = (x % rows + rows) % rows;
+                var nJ = (y % cols + cols) % cols;
+                var cell = universe[nI][nJ];
+                if (x != pX || y != pY ) {
+                    sum += cell.state;
+                }
+                // BOUNDS ((x != pX || y != pY) && x >= 0 && x < rows && y >= 0 && y < rows) {
             }
-            // BOUNDS ((x != pX || y != pY) && x >= 0 && x < rows && y >= 0 && y < rows) {
         }
     }
-    //console.log(sum);
+    else {
+        for (let x = pX - order; x <= pX + order; x++) {
+            for (let y = pY - order; y <= pY + order; y++) {
+                mh_distance = abs(pX - x) + abs(pY - y)
+                if (mh_distance <= order) {
+                    var nI = (x % rows + rows) % rows;
+                    var nJ = (y % cols + cols) % cols;
+                    var cell = universe[nI][nJ];
+                    if (x != pX || y != pY ) {
+                        sum += cell.state;
+                    }
+                    // BOUNDS ((x != pX || y != pY) && x >= 0 && x < rows && y >= 0 && y < rows) {
+                }
+            }
+        }
+    }
+    if (center == true) {
+        var centerState = universe[pX][pY];
+        sum += centerState.state;
+    }
     return sum;
 }
--- main/sketch.js
+++ main/sketch.js
@@ -1,10 +1,15 @@
 const ALIVE = 1;
 const DEAD = 0;
 
+let center = false;
+let order = 1;
 let state;
 let rows;
 let cols;
 let resolution = 10;
+
+let runMoore = false;
+let runVon = false;
 
 function make2DArray(rows, cols) {
 	var arr = new Array(rows);
@@ -14,9 +19,46 @@ function make2DArray(rows, cols) {
 	return arr;
   }
 
+function resetCanvas() {
+	clear();
+	background(0);
+	rows = floor(width / 10);
+	cols = floor(height / 10);
+	universe = make2DArray(rows, cols);
+	for (let x = 0; x < rows; x++) {
+		for (let y = 0; y < cols; y++) {
+			let rnd = floor(random(2));
+			if (rnd == 1) state = ALIVE;
+			if (rnd == 0) state = DEAD;
+			universe[x][y] = new Cell(x * resolution, y * resolution, resolution, state);
+		}
+	}
+}
+
 function setup() {
-	var golCanvas = createCanvas(windowWidth + 1, windowHeight + 1);
+	var golCanvas = createCanvas(701, 501);
 	golCanvas.parent("gol");
+
+	let type_moore_test = select("#moore");
+	type_moore_test.mousePressed(moore);
+
+	let type_von_test = select("#von");
+	type_von_test.mousePressed(vonneumann);
+
+	let center_checkbox = select("#centerCheck");
+	center_checkbox.mousePressed(enablecenter)
+
+	order_slider = createSlider(1, 10, 1);
+	order_slider.parent(orderslider);
+	order_slider.style('width', '100%');
+
+	var checkboxes = document.getElementsByTagName('input');
+
+	for (var i=0; i<checkboxes.length; i++)  {
+	if (checkboxes[i].type == 'checkbox')   {
+		checkboxes[i].checked = false;
+		}
+	}
 
 	background(0);
 	rows = floor(width / 10);
@@ -32,11 +74,47 @@ function setup() {
 	}
 }
 
+function enablecenter() {
+	if (this.checked()) {
+		center = false;
+	  } else {
+		center = true;
+	  }
+}
+
+function moore() {
+	resetCanvas();
+	runVon = false;
+	runMoore = true;
+}
+
+function vonneumann() {
+	resetCanvas();
+	runMoore = false;
+	runVon = true;
+}
+
+
 function draw() {
-	for (let x = 0; x < rows; x++) {
-		for (let y = 0; y < cols; y++) {
-		  universe[x][y].show();
+
+	order = order_slider.value();
+	document.getElementById("ordernum").innerHTML = order;
+
+	if (runMoore) {
+		for (let x = 0; x < rows; x++) {
+			for (let y = 0; y < cols; y++) {
+				universe[x][y].show();
+			}
 		}
+		nextGeneration("moore");
 	}
-	nextGeneration();
+	if(runVon) {
+		//do this
+		for (let x = 0; x < rows; x++) {
+			for (let y = 0; y < cols; y++) {
+				universe[x][y].show();
+			}
+		}
+		nextGeneration("von");
+	}
 }
